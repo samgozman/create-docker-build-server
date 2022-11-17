95ebde73eca1f5f36aed6cb7411f784e3b2e8ad1
samgozman <sam@gozman.space>
2022-11-17 16:11:43 +0400

    Create basic sh script

---
+++ builder.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+echo "This script will create or destroy docker builder server in the Hetzner Cloud."
+echo "Make sure you have created 'secrets.auto.tfvars' file with your token!"
+echo ""
+echo "Choose an option:"
+echo "1. Create builder server"
+echo "2. Destroy builder server"
+echo "3. Exit"
+echo ""
+
+echo "Enter your choice: "
+read -r action
+
+case $action in
+  1)
+    # Create builder server
+    echo "Creating builder server..."
+    terraform init
+    touch output.tmp
+    terraform apply -auto-approve > output.tmp
+    echo "Docker builder server created with IP: $server_url"
+    # Get url from Terraform output
+    server_url=$(cat output.tmp | sed -n 's/.*"docker-builder" = //p' | sed -r "s/\\\"//g")
+    # Save url to ENV
+    export DOCKER_HOST="ssh://root@$server_url"
+    echo "DOCKER_HOST updated with new server url."
+    # Remove tmp file
+    rm output.tmp
+    echo "Tap this to reconnect in another shell:"
+    echo "export DOCKER_HOST=ssh://root@$server_url"
+    echo "Creating docker builder completed."
+    exit
+    ;;
+  2)
+    # Destroy builder server
+    echo "Destroying builder server..."
+    terraform destroy -auto-approve
+    echo "Docker builder server destroyed."
+    ;;
+  3)
+    exit
+    ;;
+  *)
+    exit
+    ;;
+esac
